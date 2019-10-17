68c18f526f912b8884e52d30b2fb546040cd96e9
zhaohao <zhaohao.zeng93@gmail.com>
2019-10-18 02:59:53 +0900

    use imap instead of map

--- matchzoo/data_pack/data_pack.py
+++ matchzoo/data_pack/data_pack.py
@@ -23,16 +23,16 @@ def _apply_on_row(func, data_subset):
 
 def _parallelize_on_rows(data, func, num_of_processes=-1, verbose=1, desc=None,
                          batch_size=10):
+    n_batch = math.ceil(len(data) / batch_size)
     data_splits = filter(lambda x: len(x),
-                         np.array_split(data,
-                                        math.ceil(len(data) / batch_size)))
+                         np.array_split(data,n_batch ))
     row_func = functools.partial(_apply_on_row, func)
     if num_of_processes == -1:
         pool = mp.Pool()
     else:
         pool = mp.Pool(num_of_processes)
-    result = pd.concat(tqdm(pool.map(row_func, data_splits), desc=desc,
-                            disable=not bool(verbose)))
+    result = pd.concat(tqdm(pool.imap(row_func, data_splits), desc=desc,
+                            disable=not bool(verbose), total=n_batch))
     pool.close()
     pool.join()
     return result
